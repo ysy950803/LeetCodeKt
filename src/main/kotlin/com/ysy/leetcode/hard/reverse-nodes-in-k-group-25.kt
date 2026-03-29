620e7bd9c70060950c2525ecf2cbf271033c3fee
MeowBit <admin@ysy950803.cn>
2026-03-29 14:15:21 +0800

    Update reverse-nodes-in-k-group-25.kt

--- src/main/kotlin/com/ysy/leetcode/hard/reverse-nodes-in-k-group-25.kt
+++ src/main/kotlin/com/ysy/leetcode/hard/reverse-nodes-in-k-group-25.kt
@@ -9,6 +9,7 @@ package com.ysy.leetcode.hard
  *     var next: ListNode? = null
  * }
  */
+
 class Solution25 {
 
     class ListNode(var `val`: Int) {
@@ -16,7 +17,41 @@ class Solution25 {
     }
 
     fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
-        if (head == null || k < 1) return head
+        if (head == null || k <= 1) return head
+
+        var tail = head
+        repeat(k) {
+            if (tail == null) return head
+            tail = tail.next
+        }
+
+        val newHead = reverseList(head, tail)
+        head.next = reverseKGroup(tail, k)
+        return newHead
+    }
+
+    private fun reverseList(start: ListNode?, end: ListNode?): ListNode? {
+        var pre = end
+        var cur = start
+        while (cur != end) {
+            val next = cur?.next
+            cur?.next = pre
+            pre = cur
+            cur = next
+        }
+        return pre
+    }
+}
+
+class Solution25_2 {
+
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+    fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
+        if (head == null || k <= 1) return head
+
         val vNode = ListNode(0)
         vNode.next = head
         var pre : ListNode? = vNode
